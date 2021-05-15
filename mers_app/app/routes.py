6590a279d13d1cd778ef0942d0ce80672e1728dc
NarayanAdithya <narayanadithya1234@gmail.com>
2021-05-15 19:51:16 +0530

    Changed

--- mers_app/app/routes.py
+++ mers_app/app/routes.py
@@ -6,3 +6,18 @@ from flask import request,redirect,url_for,render_template
 @app.route('/home')
 def home():
     return render_template('index.html')
+
+
+@app.route('/registration')
+def register():
+    return render_template('Registration.html')
+    
+
+@app.route('/contactus')
+def contact():
+    return render_template('Contact.html')
+
+
+@app.route('/chatpage')
+def chatpage():
+    return render_template('Chat.html')
